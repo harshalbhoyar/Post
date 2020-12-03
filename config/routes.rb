a87d10e98e629b85a61a623387ebf38f2466253a
Harshal Bhoyar <harshalbhoyar14@gmail.com>
2020-12-03 13:13:28 +0530

    Added controller welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :comments
   resources :postts
 
   get 'more_info', to: 'postts#more'
- # root 'postts#index'
+  root 'welcome#index'
+  #root 'postts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
